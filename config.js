995c90aa5da9547a6718651703359c211d6434e2
الفخم كاتم الاحزان <mghdwrfyh@gmail.com>
2025-06-14 20:07:31 +0300

    Update config.js
    بوت واتساب

--- config.js
+++ config.js
@@ -19,9 +19,9 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['6285875158363']
-global.ownernomer = "6285875158363"
-global.premium = ['6285875158363']
+global.owner = ['967735642443']
+global.ownernomer = "967735642443"
+global.premium = ['967735642443']
 global.packname = 'Sticker By'
 global.author = 'Naze Dev'
 global.sessionName = 'nazedev'  //jangan diganti bro nanti error
